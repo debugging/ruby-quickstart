c19aadc38028ab56f3337ec230f713b24584d0d2
Salman Ahmed <salman@debugging.com>
2011-05-26 11:22:37 -0400

    dir listing and sorting

--- sysadmin.rb
+++ sysadmin.rb
@@ -11,3 +11,8 @@ ruby -i.bak -pe '$_ = "#" + $_' *.rb
 ruby -a -F, -i -ne 'puts $F.values_at(1,2,3).join("\t")' some_file.rb
 
 ruby -e 'puts Dir["*.rb"].find{|f| File.size(f) < 2048}.sort_by{|f| File.mtime(f)}'
+
+ruby -e 'system "clear; ls -lahG" while sleep 1'  #watch
+
+ruby -rFileutils -e 'f = ARGV[0]; FileUtils.mv(f, f + Time.now.strftime(".%y%m%dT%H%M%S") if File.exists?(f) and File.size(f) > 1024' /path/to/file.log
+
