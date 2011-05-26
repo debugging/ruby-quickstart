431e4884e65a3d11c48d772224a61091f69fdece
Salman Ahmed <salman@debugging.com>
2011-05-26 11:06:07 -0400

    dir listing and sorting

--- sysadmin.rb
+++ sysadmin.rb
@@ -2,8 +2,12 @@
 # $. == $<.lineno
 # $F fields
 ruby --help
+
+# -O15 == carriage return \r is 015; man ascii;
+
 ruby -e 'puts "hello, world!"'
 ruby -pe '$_ = "#" + $_' some_file.rb
 ruby -i.bak -pe '$_ = "#" + $_' *.rb
 ruby -a -F, -i -ne 'puts $F.values_at(1,2,3).join("\t")' some_file.rb
 
+ruby -e 'puts Dir["*.rb"].find{|f| File.size(f) < 2048}.sort_by{|f| File.mtime(f)}'
