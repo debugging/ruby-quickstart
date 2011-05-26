b07f342ce2b090cecbd2230d142e80be068d7a72
Salman Ahmed <salman@debugging.com>
2011-05-26 16:47:12 -0400

    .

--- files.rb
+++ files.rb
@@ -4,4 +4,25 @@ File.open("/path/to/file", w) do |f|
   f.flock(File::LOCK_UN)
 end
 
+File.open("/path/to/file", w) do |f|
+  if f.flock(File::LOCK_EX | File::LOCK_NB)
+    #...
+    f.flock(File::LOCK_UN)
+  else warn "couldn't lock"
+  end
+end
 
+require "fileutils"
+require "tempfile"
+
+class File
+  def File.open_using_temp(path)
+    res, tmp_path = nil, nil
+    Tempfile.open("....") do |f|
+      result = yeild f
+      tmp_path = f.path
+    end
+    FileUtils.move(tmp_path, path)
+    res
+  end
+end
